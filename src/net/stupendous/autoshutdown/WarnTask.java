3eac7e6f3b587af9a42dfa20d8599625541d2354
makskay <maxkreminski@gmail.com>
2012-11-14 19:04:20 -0500

    Update to Bukkit 1.4.2-R0.2
    Make shutdown warning messages configurable

--- src/net/stupendous/autoshutdown/WarnTask.java
+++ src/net/stupendous/autoshutdown/WarnTask.java
@@ -4,17 +4,57 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 import net.stupendous.autoshutdown.misc.Log;
-import net.stupendous.autoshutdown.misc.Util;
+import net.stupendous.autoshutdown.misc.Util; 
+
+import org.bukkit.configuration.file.FileConfiguration;
 
 public class WarnTask extends TimerTask {
 	protected AutoShutdownPlugin plugin = null;
 	protected Log log = null;
 	protected long seconds = 0;
+	protected String msgShutdownNow = "Server is shutting down NOW!";
+	protected String msgShutdownXSeconds = "Server is shutting down in %d seconds ...";
+	protected String msgShutdown1Minute = "Server is shutting down in 1 minute ...";
+	protected String msgShutdown1MinuteXSeconds = "Server is shutting down in 1 minute %d seconds ...";
+	protected String msgShutdownXMinutes = "Server is shutting down in %d minutes ...";
+	protected String msgShutdownXMinutesXSeconds = "Server is shutting down in %d minutes %d seconds ...";
 	
 	WarnTask(AutoShutdownPlugin instance, long seconds) {
 		plugin = instance;
 		log = plugin.log;
 		this.seconds = seconds;
+
+		FileConfiguration config = plugin.getConfig();
+		String configMsgShutdownNow = config.getString("shutdownnow");
+		String configMsgShutdownXSeconds = config.getString("shutdownXseconds");
+		String configMsgShutdown1Minute = config.getString("shutdown1minute");
+		String configMsgShutdown1MinuteXSeconds = config.getString("shutdown1minuteXseconds");
+		String configMsgShutdownXMinutes = config.getString("shutdownXminutes");
+		String configMsgShutdownXMinutesXSeconds = config.getString("shutdownXminutesXseconds");
+
+		if (configMsgShutdownNow != null) {
+			msgShutdownNow = configMsgShutdownNow;
+		}
+
+		if (configMsgShutdownXSeconds != null) {
+			msgShutdownXSeconds = configMsgShutdownXSeconds;
+		}
+
+		if (configMsgShutdown1Minute != null) {
+			msgShutdown1Minute = configMsgShutdown1Minute;
+		}
+
+		if (configMsgShutdown1MinuteXSeconds != null) {
+			msgShutdown1MinuteXSeconds = configMsgShutdown1MinuteXSeconds;
+		}
+
+		if (configMsgShutdownXMinutes != null) {
+			msgShutdownXMinutes = configMsgShutdownXMinutes;
+		}
+
+		if (configMsgShutdownXMinutesXSeconds != null) {
+			msgShutdownXMinutesXSeconds = configMsgShutdownXMinutesXSeconds;
+		}
 	}
 	
 	public void run() {
@@ -23,18 +63,18 @@ public class WarnTask extends TimerTask {
 				if (TimeUnit.SECONDS.toMinutes(seconds) > 0) {
 					if (TimeUnit.SECONDS.toMinutes(seconds) == 1) {
 						if (seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds)) == 0) {
-							Util.broadcast("Server is shutting down in 1 minute ...");
+							Util.broadcast(msgShutdown1Minute);
 						} else {
-							Util.broadcast("Server is shutting down in 1 minute %d seconds ...",
+							Util.broadcast(msgShutdown1MinuteXSeconds,
 									seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds)));
 						}
 						
 					} else {
 						if (seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds)) == 0) {
-							Util.broadcast("Server is shutting down in %d minutes ...", 
+							Util.broadcast(msgShutdownXMinutes, 
 									TimeUnit.SECONDS.toMinutes(seconds));
 						} else {
-							Util.broadcast("Server is shutting down in %d minutes %d seconds ...", 
+							Util.broadcast(msgShutdownXMinutesXSeconds, 
 									TimeUnit.SECONDS.toMinutes(seconds),
 									seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds)));
 						}
@@ -42,9 +82,9 @@ public class WarnTask extends TimerTask {
 					}
 				} else {
 					if (TimeUnit.SECONDS.toSeconds(seconds) == 1) {
-						Util.broadcast("Server is shutting down NOW!");
+						Util.broadcast(msgShutdownNow);
 					} else {
-						Util.broadcast("Server is shutting down in %d seconds ...",	seconds);
+						Util.broadcast(msgShutdownXSeconds,	seconds);
 					}
 				}
 			}
